46e43d515eb0c54f0f31a404ed11c0f6bf115ade
Yuan Zhang <63809399+YZhang469@users.noreply.github.com>
2023-03-03 12:11:29 -0500

    Update diagnostics.r

--- application/diagnostics.r
+++ application/diagnostics.r
@@ -4,8 +4,6 @@ library(ggplot2)
 library(dplyr)
 library(survRM2)
 
-source("../function/estBeta.r")
-
 ## data cleaning
 dat <- read_sas("postlt_yuan_15jul2022.sas7bdat")
 dat <- dat[dat$status_1 == 0 & dat$AGE_DON >= 12, ]

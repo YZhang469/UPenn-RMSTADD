c14642f088f368f10ca8d23ff46e1ebc748b5ac5
Yuan Zhang <63809399+YZhang469@users.noreply.github.com>
2023-04-28 15:06:49 -0400

    Update sim_supp.r

--- simulation/sim_supp.r
+++ simulation/sim_supp.r
@@ -107,4 +107,4 @@ Ls = c(2.5,5)
 for (k in 1:length(Ls)){
   res.final <- rbind.data.frame(res.final, sim.p(L = Ls[k], p = 15, n.sim = 1000))
 }
-write.csv(res.final, "res_p.csv", row.names = FALSE)
+write.csv(res.final, "./results/TableB1.csv", row.names = FALSE)
